eb0dfd835f4b84ca3b230b49a78fab5094f2316e
Anna Williamson <acwilliamson97@gmail.com>
2020-06-22 14:52:46 -0700

    insert sample data into mysql tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,27 +3,24 @@ CREATE DATABASE employees_DB;
 USE employees_DB;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    PRIMARY KEY(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT,
-    title VARCHAR(50) NOT NULL,
-	salary INT,
-    department_id INT,
-	PRIMARY KEY(id),
-	FOREIGN KEY(department_id) REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(90, 2),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-	role_id INT,
-    manager_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY (role_id) REFERENCES role (id),
-    FOREIGN KEY (manager_id) REFERENCES employee (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (name) VALUES ("Information Technology");
+
+INSERT INTO role (title, salary, department_id) VALUES ("Engineer", "85000", "1");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Anna", "Williamson", "1", "1");
